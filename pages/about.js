c49bc0d37f634e5dcb85e3c61c325b5097d9a7bf
kelli <kellikells@gmail.com>
2021-01-25 21:58:34 -0800

    added text shadow via tailwindcss-textshadow package

--- pages/about.js
+++ pages/about.js
@@ -4,16 +4,13 @@ const About = () => (
         <div className='px-5 mt-8 sm:mt-16  md:mt-32 flex flex-col flex-wrap space-y-16 md:space-y-0 patterns'>
                 <div className='mb-auto md:space-y-5 md:grid grid-cols-2'>
                         <div>
-                                <h1 className='text-8xl '><span>Hi, I'm </span><span className='font-bold'>Kelli.</span></h1>
+                                <h1 className='text-8xl '><span>Hi, I'm </span><span className='font-bold '>Kelli.</span></h1>
+
                                 <br />
-                                
-                                {/* subhead: 2 views */}
-                                <div className='contents'>
-                                        <h3 className='text-3xl tracking-widest font-bold text-gray-600 my-10 md:my-5 md:text-center'>A Full-Stack Web Developer</h3>
-                                </div>
                           
-                               
+                                <h3 className='text-3xl tracking-widest font-bold text-gray-600 my-10 md:my-5  lg:text-center text-shadow-lg'>A Full-Stack Web Developer</h3>         
                         </div>
+
                         <div className='lg:ml-10'>
                                 <p className='tracking-wide leading-relaxed f-ubuntu'>
                                  Los Angeles based developer with a passion for creating simple and efficient code. 
@@ -24,11 +21,7 @@ const About = () => (
                                 I believe that every problem has an answer, and I enjoy being able to provide the solution.{' '}
                                 </p>
                         </div>
-
-
-
                 </div>
-
         </div>
 );
 
